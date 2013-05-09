03ecf49cba8cf193711ae22469fbf15e00c540f2
Tristan Rivoallan <tristan@rivoallan.net>
2013-05-09 11:40:19 +0200

    [front] câblage des liens

--- src/public/index.php
+++ src/public/index.php
@@ -13,6 +13,11 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
 
+// Named routes (@see http://silex.sensiolabs.org/doc/providers/url_generator.html)
+$app->register(new Provider\UrlGeneratorServiceProvider());
+
+// Controller classes (required by Web Profiler - @see http://silex.sensiolabs.org/doc/providers/service_controller.html)
+$app->register(new Provider\ServiceControllerServiceProvider());
 
 // Debugging features. TODO : make does switchable
 
@@ -20,33 +25,34 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 $app['debug'] = true;
 
 // Web Profiler (@see https://github.com/sensiolabs/Silex-WebProfiler)
-$app->register(new Provider\ServiceControllerServiceProvider());
-$app->register(new Provider\UrlGeneratorServiceProvider());
 $app->register(new Provider\WebProfilerServiceProvider(), array(
     'profiler.cache_dir' => __DIR__.'/../cache/profiler',
     'profiler.mount_prefix' => '/_profiler', // this is the default
 ));
 
-
 // Homepage
 $app->get('/', function(Silex\Application $app) {
     return $app['twig']->render('home.twig.html');
-});
+})
+->bind('homepage');
 
 // About page
 $app->get('/apropos', function(Silex\Application $app) {
     return $app['twig']->render('apropos.twig.html');
-});
+})
+->bind('apropos');
 
 // Links
 $app->get('/liens', function(Silex\Application $app) {
     return $app['twig']->render('liens.twig.html');
-});
+})
+->bind('liens');
 
 // Shows list
 $app->get('/emissions', function(Silex\Application $app) {
     return $app['twig']->render('emissions.twig.html');
-});
+})
+->bind('emissions');
 
 // Show page
 $app->get('/emission/{id}', function(Silex\Application $app, $id) {
@@ -62,7 +68,8 @@ $app->get('/emission/{id}', function(Silex\Application $app, $id) {
 	);
 
     return $app['twig']->render('emission.twig.html', array('show' => $show));
-});
+})
+->bind('emission');
 
 // Run application
 $app->run();

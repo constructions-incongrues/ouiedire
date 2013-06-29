9d6aedefc4914cad6d3e36191035aed53c259045
Tristan Rivoallan <tristan@rivoallan.net>
2013-06-30 00:28:47 +0200

    [smartplaylist] homogénéisation et namespacisation des directives de configuration

--- src/public/assets/javascripts/mep-feature-smartplaylist.js
+++ src/public/assets/javascripts/mep-feature-smartplaylist.js
@@ -1,12 +1,18 @@
+/**
+ * Smart playlists
+ * 
+ * Links a timestamped playlist to player states.
+ */
 (function($) {
 
   // Defaults
   $.extend(mejs.MepDefaults, {
-    smartplaylistCurrentClass:   'me-smartplaylist-current',
-    smartplaylistLinkTitle:      'Listen to this track',
-    smartplaylistSelector:       '.me-smartplaylist-playlist',
-    smartplaylistTimestampRegex: '(\\d{2}):(\\d{2}):(\\d{2})',
-    smartplaylistPositionQueryVar: 'me-smartplaylist-position',
+    smartplaylistCurrentClass:       'mejs-smartplaylist-current',
+    smartplaylistLinkTitle:          'Listen to this track',
+    smartplaylistSelectorTimestamp:  'a.mejs-smartplaylist-time',
+    smartplaylistSelectorPlaylist:   '.mejs-smartplaylist-playlist',
+    smartplaylistTimestampRegex:     '(\\d{2}):(\\d{2}):(\\d{2})',
+    smartplaylistPositionQueryVar:   'mejs-smartplaylist-position',
     smartplaylistTimeFactors: { 
       1: 60 * 60, // Hours
       2: 60,      // Minutes
@@ -17,11 +23,11 @@
   $.extend(MediaElementPlayer.prototype, {
     buildsmartplaylist: function(player, controls, layers, media) {
       var $this = $(this)[0];
-      var $playlist = $($this.options.smartplaylistSelector);
+      var $playlist = $($this.options.smartplaylistSelectorPlaylist);
       var factors = $this.options.smartplaylistTimeFactors;
 
       // Analyze playlist timestamps
-      $playlist.find('a.time').each(function() {
+      $playlist.find($this.options.smartplaylistSelectorTimestamp).each(function() {
         // Convert to seconds
         var seconds = 0;
         var parts = this.text.match(new RegExp($this.options.smartplaylistTimestampRegex));
@@ -29,22 +35,22 @@
         seconds += parseInt(parts[2]) * factors['2'];
         seconds += parseInt(parts[3]) * factors['3'];
         $(this).attr('href', '?' + $this.options.smartplaylistPositionQueryVar + '=' + seconds);
-        $(this).data('seconds', seconds);
+        $(this).data('mejs-smartplaylist-seconds', seconds);
         if ($(this).attr('title') == undefined) {
           $(this).attr('title', $this.options.smartplaylistLinkTitle);
         }
       });
 
       // Clicking on a timestamps seeks to the appropriate time in mix
-      $playlist.find('a.time').on('click', function() {
-        $playlist.find('a.time').removeClass($this.options.smartplaylistCurrentClass);
+      $playlist.find($this.options.smartplaylistSelectorTimestamp).on('click', function() {
+        $playlist.find($this.options.smartplaylistSelectorTimestamp).removeClass($this.options.smartplaylistCurrentClass);
         $(this).addClass($this.options.smartplaylistCurrentClass);
         var player = mejs.players.mep_0;
         if (player.media.paused) {
-          window.position = $(this).data('seconds');
+          window.position = $(this).data('mejs-smartplaylist-seconds');
           mejs.players.mep_0.play();      
         } else {
-          player.setCurrentTime($(this).data('seconds'));
+          player.setCurrentTime($(this).data('mejs-smartplaylist-seconds'));
         }
 
         return false;
@@ -52,11 +58,11 @@
 
       // Update current track
       media.addEventListener('timeupdate', function() {
-        var times = $('a.time');
+        var times = $($this.options.smartplaylistSelectorTimestamp);
         times.each(function(i) {
           // Boundaries
-          var timeMin = parseInt($(this).data('seconds'));
-          var timeMax = $(times[i + 1]).data('seconds');
+          var timeMin = parseInt($(this).data('mejs-smartplaylist-seconds'));
+          var timeMax = $(times[i + 1]).data('mejs-smartplaylist-seconds');
           if (timeMax == undefined) {
             timeMax = 666666666666666; // L'infini, en moins glorieux
           }
@@ -64,7 +70,7 @@
 
           // Test if track is within boundaries
           if (timeCurrent > timeMin && timeCurrent < timeMax) {
-            $('a.time').removeClass($this.options.smartplaylistCurrentClass);
+            $($this.options.smartplaylistSelectorTimestamp).removeClass($this.options.smartplaylistCurrentClass);
             $(this).addClass($this.options.smartplaylistCurrentClass);
           }
         });

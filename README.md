7a0f2545afb1f3636809f85ceffff3ceb69ab4ff
Tristan Rivoallan <tristan.rivoallan@aramisauto.com>
2015-01-14 10:35:45 +0100

    [dev] Compatibilité Vagrant

--- README.md
+++ README.md
@@ -7,8 +7,17 @@ Installation
 ============
 
 ```bash
-git clone git@github.com:constructions-incongrues/net.ouiedire.www.git
+sudo apt-get install virtualbox resolvconf dnsmasq
+wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb
+sudo dpkg -i vagrant_1.7.2_x86_64.deb
+
+git clone https://github.com/constructions-incongrues/net.ouiedire.www.git
 cd net.ouiedire.www
-./composer.phar update
-ant configure build
+vagrant plugin install vagrant-vbguest
+vagrant plugin install vagrant-share
+vagrant plugin install landrush
+vagrant up
+
+sudo sh -c 'echo "server=/vagrant.dev/127.0.0.1#10053" > /etc/dnsmasq.d/vagrant-landrush'
+sudo service dnsmasq restart
 ```
---
+++ provision.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Mise à jour des dépots
+apt-get -qq update
+
+# Configuration de la timezone
+echo "Europe/Paris" > /etc/timezone 
+apt-get install -y tzdata
+dpkg-reconfigure -f noninteractive tzdata
+
+# Installation de Apache et PHP
+apt-get -y install libapache2-mod-php5 php5-cli
+
+# Configuration de Apache
+sed -i s/AllowOverride\ None/AllowOverride\ All/g /etc/apache2/sites-enabled/000-default
+a2enmod rewrite
+a2enmod expires
+service apache2 reload
+
+# Configuration du projet
+apt-get install -y ant 
+cd /vagrant
+./composer.phar install --prefer-dist
+ant configure build -Dprofile=vagrant
+
+# Mise à disposition du projet dans Apache
+ln -sf /vagrant/src/public/* /var/www/
+rm -f /var/www/index.html
+
+# Informations
+echo 
+echo -e "Le site est disponible à l'adresse : http://ouiedire.vagrant.dev/app_dev.php"

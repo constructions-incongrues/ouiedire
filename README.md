96e64b1e2902e74b62bb2916945993370f2731ea
Tristan Rivoallan <tristan.rivoallan@aramisauto.com>
2015-02-17 15:53:54 +0100

    [dev] ajout du module cloudflare et passage en vagrant only

--- README.md
+++ README.md
@@ -8,8 +8,11 @@ Installation
 
 ```bash
 sudo apt-get install virtualbox resolvconf dnsmasq
-wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb
-sudo dpkg -i vagrant_1.7.2_x86_64.deb
+sudo sh -c 'echo "server=/vagrant.dev/127.0.0.1#10053" > /etc/dnsmasq.d/vagrant-landrush'
+sudo service dnsmasq restart
+
+wget https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.1_x86_64.deb
+sudo dpkg -i vagrant_1.7.1_x86_64.deb
 
 git clone https://github.com/constructions-incongrues/net.ouiedire.www.git
 cd net.ouiedire.www
@@ -17,7 +20,4 @@ vagrant plugin install vagrant-vbguest
 vagrant plugin install vagrant-share
 vagrant plugin install landrush
 vagrant up
-
-sudo sh -c 'echo "server=/vagrant.dev/127.0.0.1#10053" > /etc/dnsmasq.d/vagrant-landrush'
-sudo service dnsmasq restart
 ```
--- provision.sh
+++ provision.sh
@@ -4,9 +4,12 @@
 apt-get -qq update
 
 # Configuration de la timezone
-echo "Europe/Paris" > /etc/timezone 
+echo "Europe/Paris" > /etc/timezone
 apt-get install -y tzdata
 dpkg-reconfigure -f noninteractive tzdata
+
+# Installation de Java
+apt-get -y install openjdk-7-jre-headless
 
 # Installation de Apache et PHP
 apt-get -y install libapache2-mod-php5 php5-cli
@@ -18,7 +21,7 @@ a2enmod expires
 service apache2 reload
 
 # Configuration du projet
-apt-get install -y ant 
+apt-get install -y ant
 cd /vagrant
 ./composer.phar install --prefer-dist --no-progress
 ant configure build -Dprofile=vagrant
@@ -28,5 +31,5 @@ ln -sf /vagrant/src/public/* /var/www/
 rm -f /var/www/index.html
 
 # Informations
-echo 
+echo
 echo -e "Le site est disponible à l'adresse : http://ouiedire.vagrant.dev/app_dev.php"

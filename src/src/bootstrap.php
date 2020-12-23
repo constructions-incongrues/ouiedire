5087ff44a23c1b1f708fef3e01f4ae037882cf28
Damien Gosset <zobzz666@gmail.com>
2020-12-23 17:32:20 +0100

    supression systeme de cache

--- src/src/bootstrap.php
+++ src/src/bootstrap.php
@@ -271,12 +271,12 @@ function getShows(Silex\Application $app, $preview = false, $artist = null) {
     $pathData = __DIR__.'/../data';
 
     // cache
-    $cacheFile = $pathData.'/cache/shows_'.$preview.'_'.$artist.'.txt';
+    /*$cacheFile = $pathData.'/cache/shows_'.$preview.'_'.$artist.'.txt';
     if(file_exists($cacheFile) && time()-filemtime($cacheFile) < 3600){
         $current = file_get_contents($cacheFile);        
         $shows = unserialize($current);
     }
-    else{
+    else{*/
         // Search for shows manifests
         $finder = new Finder();
         $finder = $finder
@@ -326,7 +326,7 @@ function getShows(Silex\Application $app, $preview = false, $artist = null) {
         $current = file_get_contents($cacheFile);
         $current = serialize($shows);
         file_put_contents($cacheFile, $current);        
-    }
+    //}
     return $shows;
 }
 
